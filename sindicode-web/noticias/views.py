56eaf8cddc8be9d8555b97ed89c3758f316139e2
LeoSousaJesus <leandrosousajesus@gmail.com>
2025-10-20 10:55:46 -0300

    Ajustando arquivos Django

--- sindicode-web/noticias/views.py
+++ sindicode-web/noticias/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+# função
+# se def dentro da classe = metodo
+# se def fora da classe = função
+def index(request):
+    return HttpResponse("<h1><marquee>Alô Django</marquee></h1>")
 
 # Create your views here.
